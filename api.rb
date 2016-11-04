0ae5278ef087a7f7d930aa12dfa95dd98bb88691
Carmen Barron <carmen.barron@gmail.com>
2016-11-04 18:41:22 -0400

    fix case of httparty in Gemfile

--- api.rb
+++ api.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'HTTParty'
+require 'httparty'
 
 before do
   new_report =  HTTParty.get("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202450080&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
